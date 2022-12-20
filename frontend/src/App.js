e5fa17822a05ad5db2fe66ec216c70bfba496993
justintsugranes <justintsugranes@proton.me>
2022-12-20 07:06:58 -0500

    add edit user and edit notes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
-import { Routes, Route } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
+import DashLayout from './components/DashLayout'
 import Layout from './components/Layout'
 import Public from './components/Public'
 import Login from './features/auth/Login'
-import DashLayout from './components/DashLayout'
 import Welcome from './features/auth/Welcome'
 import NotesList from './features/notes/NotesList'
 import UsersList from './features/users/UsersList'
+import EditUser from './features/users/EditUser'
+import NewUserForm from './features/users/NewUserForm'
+import EditNote from './features/notes/EditNote'
+import NewNote from './features/notes/NewNote'
 
 function App() {
   return (
@@ -17,12 +21,16 @@ function App() {
         {/* protected routes */}
         <Route path='dash' element={<DashLayout />}>
           <Route index element={<Welcome />} />
-          <Route path='notes'>
-            <Route index element={<NotesList />} />
-          </Route>
           <Route path='users'>
             <Route index element={<UsersList />} />
+            <Route path=':id' element={<EditUser />} />
+            <Route path=':new' element={<NewUserForm />} />
           </Route>
+        </Route>
+        <Route path='notes'>
+          <Route index element={<NotesList />} />
+          <Route path=':id' element={<EditNote />} />
+          <Route path='new' element={<NewNote />} />
         </Route>
         {/* end dash */}
       </Route>
